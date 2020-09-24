2708804a95fa6ad0514a5b76b6b98a9e6fdafd2d
Phattaraphon Pewruang <phattaraphon.pe.61@ubu.ac.th>
2020-09-24 20:45:09 +0700

    homepage

---
+++ src/components/layout/MainNavbar/MainNavbar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { Container, Navbar } from "shards-react";
+
+import NavbarSearch from "./NavbarSearch";
+import NavbarNav from "./NavbarNav/NavbarNav";
+import NavbarToggle from "./NavbarToggle";
+
+
+const MainNavbar = ({ layout, stickyTop }) => {
+  const classes = classNames(
+    "main-navbar",
+    "bg-white",
+    stickyTop && "sticky-top"
+  );
+
+  return (
+    <div className={classes}>
+      {/* <Container> */}
+        <Navbar type="light" className="align-items-stretch flex-xs-nowrap p-0 d-flex justify-content-end">
+
+          {/* <NavbarSearch /> */}
+          <NavbarNav />
+          {/* <NavbarToggle /> */}
+        </Navbar>
+      {/* </Container> */}
+    </div>
+  );
+};
+
+MainNavbar.propTypes = {
+  /**
+   * The layout type where the MainNavbar is used.
+   */
+  layout: PropTypes.string,
+  /**
+   * Whether the main navbar is sticky to the top, or not.
+   */
+  stickyTop: PropTypes.bool
+};
+
+MainNavbar.defaultProps = {
+  stickyTop: true
+};
+
+export default MainNavbar;
